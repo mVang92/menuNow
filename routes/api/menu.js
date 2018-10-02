8333b2cc90fe3356ec14eb6d4383b39fefe5fb73
BFraven <Beltlok@gmail.com>
2018-10-01 21:15:31 -0500

    added client schema and other needed folders

---
+++ routes/api/menu.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const menuController = require("../../controllers/menuController");
+
+// Matches with "/api/articles"
+router.route("/")
+  .get(menuController.findAll)
+
+// Matches with "/api/article/:id"
+router
+  .route("/:id")
+  .get(menuController.findById)
+  .put(menuController.update)
+  .delete(menuController.remove);
+
+router
+  .route("/")
+  .post(menuController.create)
+
+module.exports = router;
