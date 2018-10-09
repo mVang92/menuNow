43b2da540482a8473df0879dc62060a0ab19a871
Brady Shinners <brady.shinners@gmail.com>
2018-10-08 21:12:21 -0500

    modal displaying

---
+++ client/src/components/Modal/SignInModal.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactModal from 'react-modal';
+
+const SignIn = props => {
+
+    return (
+        <ReactModal
+            isOpen={props.showModal}
+            contentLabel="Minimal Modal Example"
+        >
+            <div>
+                <div className="modal-header text-center">
+                    hello world
+                    </div>
+                <div className="modal-body">
+                    ...
+                    </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" onClick={props.handleCloseModal} data-dismiss="modal">Close</button>
+                    <button type="button" className="btn btn-primary">Save changes</button>
+                </div>
+
+            </div>
+
+        </ReactModal >
+
+    );
+};
+
+export default SignIn;
--- client/src/components/Nav/Navbtn.js
+++ client/src/components/Nav/Navbtn.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navbtn = props => {
     return (
-        <li className="nav-item">
-            <a className="nav-link" href="#">{props.name}</a>
+        <li {...props} className="nav-item">
+            <a {...props} className="nav-link" href="#">{props.name}</a>
         </li>
     )
 }
