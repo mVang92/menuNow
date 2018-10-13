928b6763cbc79c06ceb16b9f9f98c12b113d4a33
mVang <meng.vang92@yahoo.com>
2018-10-13 11:26:36 -0500

    merge

--- client/src/App.js
+++ client/src/App.js
@@ -140,7 +140,7 @@ export default class App extends Component {
     this.setState({
       [name]: value,
     })
-    // console.log(name, value);
+    console.log(name, value);
   };
 
   menuClick = event => {
