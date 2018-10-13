34f67ebdfd0cd3e14ee05e48e927ed64b0ae49c3
Ansel Adams <dj@volaciousbrands.com>
2018-10-13 12:13:49 -0500

    add a menu form working

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,6 +3,21 @@ import axios from "axios";
 export default {
   
   getItems: () => axios.get('/api/item'),
+  createMenu: data => {
+    console.log('HIT THIS ROUTE')
+    // Won't work if they enter more than 3 submenus
+    return (
+      axios.post(`/api/menu`, {
+        submenu: [
+          data[0],
+          data[1],
+          data[2]
+        ]
+      })
+      .then(console.log("MENU CREATED"))
+      .catch(error => { console.log(error) })
+    )
+  },
   save: data => {
     console.log(data)
     return (
