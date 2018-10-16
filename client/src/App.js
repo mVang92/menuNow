dfd9ac316cd2db69ece7eec29f21424dff0484d8
mVang <meng.vang92@yahoo.com>
2018-10-15 19:49:14 -0500

    merged createAcountModal

--- client/src/App.js
+++ client/src/App.js
@@ -129,7 +129,7 @@ export default class App extends Component {
     this.setState({
       [name]: value,
     })
-    // console.log(name, value);
+    console.log(name, value);
   };
 
   menuClick = event => {
@@ -169,6 +169,7 @@ export default class App extends Component {
 
   handleSignIn(event) {
     event.preventDefault();
+<<<<<<< HEAD
     console.log("signing in: " + this.state.email);
     auth
       .doSignInWithEmailAndPassword(this.state.email, this.state.password)
@@ -215,6 +216,32 @@ export default class App extends Component {
         this.setState({ loggedin: false });
       });
   };
+=======
+    this.handleCloseModal();
+    // console.log(this.state.email, this.state.password)
+    auth.doSignInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(() => {
+        this.setState({
+          loggedin: true
+        });
+      }
+      )
+  }
+
+  handleSignUp(event) {
+    event.preventDefault();
+    // this.handleCloseModal();
+    console.log("email: " + this.state.email);
+    console.log("password: " + this.state.password);
+    auth.doCreateUserWithEmailAndPassword(this.state.email, this.state.password)
+      // .then(() => {
+      //   this.setState({
+      //     loggedin: true
+      //   });
+      // }
+      // )
+  }
+>>>>>>> stash
 
   render() {
     return (
@@ -302,7 +329,10 @@ export default class App extends Component {
             handleCloseModal={this.handleCloseModal}
             showModal={this.state.showModal}
             handleSignIn={this.handleSignIn}
+<<<<<<< HEAD
             handleSignOut={this.handleSignOut}
+=======
+>>>>>>> stash
             handleSignUp={this.handleSignUp}
             handleChange={this.handleChange}
           />
